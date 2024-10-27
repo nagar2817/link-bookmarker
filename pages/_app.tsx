7c0c688e30bf5dcc1dbd67f284c0bbc02161d33a
Rohit Nagar <nagar.2@iitj.ac.in>
2024-10-27 08:42:34 +0000

    admin page for link creation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,14 +3,17 @@ import Layout from '../components/Layout';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client'
 import apolloClient from '../lib/apollo'
+import { UserProvider } from '@auth0/nextjs-auth0/client'
 
 function MyApp({ Component, pageProps }:AppProps) {
   return (
+    <UserProvider>
     <ApolloProvider client={apolloClient}>
     <Layout>
       <Component {...pageProps} />
     </Layout>
     </ApolloProvider>
+    </UserProvider>
   );
 }
 
